fecaef08996b0d5a241c116d2fbd472f65b47239
emerell <valeriya.mihaleva.1999@gmail.com>
2018-06-28 20:08:37 +0300

    add almost OK test for views

--- src/vlog/tests/test_views.py
+++ src/vlog/tests/test_views.py
@@ -1,11 +1,6 @@
-from django.test import TestCase, override_settings
-from django.test import Client
-import datetime
-from vlog import views
-from vlog.models import Article, Category, Comment, Tag
+from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 
 
 class VlogViewsTestCase(TestCase):
@@ -17,7 +12,7 @@ class VlogViewsTestCase(TestCase):
             password='qwerty123'
         )
 
-    def test_view_url_by_name(self):
+    def test_index_view(self):
 
         response = self.client.login(
             username='user',
@@ -27,7 +22,83 @@ class VlogViewsTestCase(TestCase):
         response = self.client.get(reverse('vlog:index'))
         self.assertEquals(response.status_code, 200)
 
-        # import ipdb
-        # ipdb.set_trace()
+        self.assertTrue('categories' in response.context_data)
+        self.assertTrue('articles' in response.context_data)
+        self.assertTrue('tags' in response.context_data)
 
-        self.assertTrue('categories' in response.context)
+        self.assertEqual([category.pk for category in response.context_data['categories']], [2, 3, 1])
+        self.assertEqual([category.title for category in
+                          response.context_data['categories']], ['Food', 'Art', 'Sport'])
+
+    def test_categories_view(self):
+
+        response = self.client.login(
+            username='user',
+            password='qwerty123'
+        )
+        self.assertTrue(response)
+        response = self.client.get(reverse('vlog:categories'))
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('categories' in response.context_data)
+        self.assertEqual([category.title for category in
+                          response.context_data['categories']], ['Food', 'Art', 'Sport'])
+
+    def test_tags_view(self):
+
+        response = self.client.login(
+            username='user',
+            password='qwerty123'
+        )
+        self.assertTrue(response)
+        response = self.client.get(reverse('vlog:tags'))
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('articles' in response.context_data)
+        self.assertTrue('tags' in response.context_data)
+        self.assertEqual([article.pk for article in
+                         response.context_data['articles']], [2, 3, 1])
+        self.assertEqual([tag.title for tag in
+                          response.context_data['tags']], ['tag3', 'tag2', 'tag1'])
+
+    def test_article_view(self):
+
+        response = self.client.login(
+            username='user',
+            password='qwerty123'
+        )
+        self.assertTrue(response)
+        response = self.client.get(reverse('vlog:article',
+                                   kwargs={'category_slug': 'sport', 'article_slug': 'about-sport'}))
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('article' in response.context_data)
+        self.assertEqual(response.context_data['article'].title, 'About sport')
+
+    def test_tag_view(self):
+
+        response = self.client.login(
+            username='user',
+            password='qwerty123'
+        )
+        self.assertTrue(response)
+        response = self.client.get(reverse('vlog:tag',
+                                           kwargs={'slug': 'tag1'}))
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('tag' in response.context_data)
+        self.assertEqual(response.context_data['tag'].title, 'tag1')
+
+    def test_tag_view(self):
+        response = self.client.login(
+            username='user',
+            password='qwerty123'
+        )
+        self.assertTrue(response)
+
+        response = self.client.get(reverse('vlog:category',
+                                           kwargs={'slug': 'art'}))
+        self.assertEquals(response.status_code, 200)
+
+        self.assertTrue('category' in response.context_data)
+        self.assertEqual(response.context_data['category'].title, 'Art')
