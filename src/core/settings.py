356c46e759e277b4f3d5f6f5994b975f37cf85aa
emerell <valeriya.mihaleva.1999@gmail.com>
2018-07-18 03:13:08 +0300

    Django logging

--- src/core/settings.py
+++ src/core/settings.py
@@ -200,6 +200,48 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 MEDIA_URL = '/media/'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'logging.NullHandler',
+        },
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': BASE_DIR + "/logfile",
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
+
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 try:
--- src/core/views.py
+++ src/core/views.py
@@ -1,10 +1,18 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
+import logging
+
+log = logging.getLogger(__name__)
 
 
 class BaseView(TemplateView):
     template_name = ''
+
+    log.debug("Hey there debug works!!")
+    log.info("Hey there info works!!")
+    log.warning("Hey there warning works!!")
+    log.error("Hey there error works!!")
 
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
